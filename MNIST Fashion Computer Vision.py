3ec270cf152213f6aaad25c1c462519a164686b1
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-05-29 12:37:48 +0100

    Define callbacks and Trainin Epochs

--- MNIST Fashion Computer Vision.py
+++ MNIST Fashion Computer Vision.py
@@ -16,6 +16,15 @@ print(training_images[29])
 training_images  = training_images / 255.0
 test_images = test_images / 255.0
 
+# Defne Calbacks to stop the training when a Loss objective is satisfied
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('loss')<0.4):
+      print("\nReached 60% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
+
 # Model Specification
 
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
@@ -27,7 +36,7 @@ model.compile(optimizer = tf.optimizers.Adam(),
               metrics=['accuracy'])
 
 # Model Training
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=20 ,callbacks=[callbacks])
 
 #Model evaluation
 model.evaluate(test_images, test_labels)
