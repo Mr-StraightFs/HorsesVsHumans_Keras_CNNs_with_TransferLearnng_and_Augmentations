a428c945da911558cd804737f6e70c3887c57f1e
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-05-29 11:55:33 +0100

    Visualize Data

--- MNIST Fashion Computer Vision.py
+++ MNIST Fashion Computer Vision.py
@@ -7,3 +7,9 @@ import matplotlib.pyplot as plt
 mnist = tf.keras.datasets.fashion_mnist
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 
+#Visualize the Data : an array image from the dataset
+plt.imshow(training_images[29])
+print(training_labels[29])
+print(training_images[29])
+
+
