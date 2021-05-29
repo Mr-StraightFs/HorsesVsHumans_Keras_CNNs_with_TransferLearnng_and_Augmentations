fff2198e99d590464652603ab15bf2b2de0ecec8
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-05-29 12:05:25 +0100

    Model Spec , Tweaking and Training

--- MNIST Fashion Computer Vision.py
+++ MNIST Fashion Computer Vision.py
@@ -16,4 +16,16 @@ print(training_images[29])
 training_images  = training_images / 255.0
 test_images = test_images / 255.0
 
+# Model Specification
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+# Loss and Gradient Parameters
+model.compile(optimizer = tf.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Model Training
+model.fit(training_images, training_labels, epochs=5)
 
