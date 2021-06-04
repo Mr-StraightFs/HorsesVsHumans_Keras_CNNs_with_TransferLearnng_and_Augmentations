789c984570becc104ab516a2d05571be0b4c026e
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-06-04 16:06:03 +0100

    Loading Packeges

---
+++ Weights_Initialization_Neural_Networks.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+from public_tests import *
+from init_utils import sigmoid, relu, compute_loss, forward_propagation, backward_propagation
+from init_utils import update_parameters, predict, load_dataset, plot_decision_boundary, predict_dec
+
